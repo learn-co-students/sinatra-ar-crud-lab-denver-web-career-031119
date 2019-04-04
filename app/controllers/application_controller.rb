23de6c9a2a5c86d49d7559707fea4c3bb785fbde
github username <github email address>
2019-04-03 21:08:43 -0600

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,46 @@ require_relative '../../config/environment'
 
 class ApplicationController < Sinatra::Base
 
+  use Rack::MethodOverride
+
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
   end
 
-  get '/' do
+  get '/articles' do
+    erb :index
   end
+
+  get '/articles/new' do
+    erb :new
+  end
+
+  post '/articles' do
+    @article = Article.create(params)
+    redirect "/articles/#{@article.id}"
+  end
+
+  get '/articles/:id' do
+    @article = Article.find(params[:id])
+    erb :show
+  end
+
+  get '/articles/:id/edit' do
+    @article = Article.find(params[:id])
+    erb :edit
+  end
+
+  patch '/articles/:id' do
+    @article = Article.update({
+    title: params[:title],
+    content: params[:content]})
+    redirect "/articles/#{@article.id}"
+  end
+
+  delete '/articles/:id' do
+    Article.delete(params[:id])
+    redirect "/articles/#{@article.id}"
+  end
+
 end
